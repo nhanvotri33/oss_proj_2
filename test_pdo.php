01a7284a3df2754dc05a10294773729a7d8792e4
nhanvotri33 <appleofcz@gmail.com>
2025-12-16 22:28:21 +0700

    fixed n3

--- test_pdo.php
+++ test_pdo.php
@@ -1,28 +1,12 @@
 <?php
-// try {
-//     $pdo = new PDO(
-//         "mysql:host=sql100.infinityfree.com;dbname=if0_40694250_book_store_1;charset=utf8mb4",
-//         "if0_40694250",
-//         "tfjeocw0xnz"
-//     );
-//     echo "✅ Database connected successfully";
-// } catch (PDOException $e) {
-//     echo $e->getMessage();
-// }
-
-
-$conn = mysqli_connect(
-    "sql100.infinityfree.com",
-    "if0_40694250",
-    "tfjeocw0xnz",
-    "if0_40694250_book_store_1"
-);
-
-if ($conn) {
+try {
+    $pdo = new PDO(
+        "mysql:host=sql100.infinityfree.com;dbname=if0_40694250_book_store_1;charset=utf8mb4",
+        "if0_40694250",
+        "tfjeocw0xnz"
+    );
     echo "✅ Database connected successfully";
-} else {
-    echo "❌ Failed";
+} catch (PDOException $e) {
+    echo $e->getMessage();
 }
-
-
 ?>
